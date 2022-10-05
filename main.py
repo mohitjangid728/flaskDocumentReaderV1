49e8fdab7a9b88f0ae59a64e6a7bf0c5a951ea1b
Mohit Jangid <57033541+mohitjangid728@users.noreply.github.com>
2022-10-05 15:27:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'hello.docx'))
-            doc = aw.Document("hello.docx")
+            # doc = aw.Document("hello.docx")
             # doc.save("Output.html")
             # file = codecs.open("Output.html", "r", "utf-8")
             # output = file.read()
